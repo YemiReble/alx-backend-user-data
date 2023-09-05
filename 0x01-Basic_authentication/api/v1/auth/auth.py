9c53c2ce885e443d2575f289c2cb1d1689d7ea4f
YemiDev <abodunrinsulaimon1@gmail.com>
2023-09-05 16:58:34 +0100

    A new class added for [Authentication]

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Authentication file
+"""
+
+
+from flask import request
+
+
+class Auth():
+    """ Authentication call declaration
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ A public method of class Auth
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ Another public method
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Current User request method
+        """
+        return None

0324c94eb2925235ac97791869b1b19b6888e4aa
YemiDev <abodunrinsulaimon1@gmail.com>
2023-09-05 15:34:32 +0100

    Implimented the Unauthorized msg If any

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,11 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('/unauthorized', strict_slashes=False)
+def unauth() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+        - if user is not authorized
+    """
+    return abort(401)

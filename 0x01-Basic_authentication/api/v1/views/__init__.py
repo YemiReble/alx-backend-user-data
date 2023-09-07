d5f1fea0a3cc060177ac4dd9eff50c44d9ba4ec3
YemiDev <abodunrinsulaimon1@gmail.com>
2023-09-07 11:29:25 +0100

    Version Update

--- 0x01-Basic_authentication/api/v1/views/__init__.py
+++ 0x01-Basic_authentication/api/v1/views/__init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """ DocDocDocDocDocDoc
 """
-# from api.v1.views.users import *
+from api.v1.views.users import *
 # Code section commented because of circular imort error
-# from api.v1.views.index import *
-from models.user import User
+from api.v1.views.index import *
+# from models.user import User
 from flask import Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")

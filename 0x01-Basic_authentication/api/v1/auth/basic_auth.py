f15c77ee9abf643acfd4fd97c1a03a8775b3e643
YemiDev <abodunrinsulaimon1@gmail.com>
2023-09-07 13:54:49 +0100

    Basic Authentication Class for User Authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Basic Authentication Class that
+that inherits from Auth Class from auth
+"""
+
+
+from auth import Auth
+
+
+class BasicAuth(Auth):
+    """ Basic Authentication Class that
+        that inherits from Auth Class from auth
+    """

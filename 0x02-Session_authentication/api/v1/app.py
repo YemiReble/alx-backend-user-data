ad080ec1977c46b4bb4dbc026b20a3e7a8c96db1
YemiDev <abodunrinsulaimon1@gmail.com>
2023-09-09 18:19:25 +0100

    Version Update

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -15,6 +15,14 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
 
+#authenticate = getenv("AUTH_TYPE")
+#if authenticate == 'auth':
+#    from api.v1.auth.auth import Auth
+#    auth = Auth()
+#if authenticate == 'basic_auth':
+#    from api.v1.auth.basic_auth import BasicAuth
+#    auth = BasicAuth()
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler

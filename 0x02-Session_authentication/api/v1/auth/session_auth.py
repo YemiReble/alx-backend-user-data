a502feb2570c58bec9fbc13b7e81d9821715a169
YemiDev <abodunrinsulaimon1@gmail.com>
2023-09-09 18:33:22 +0100

    Version Update

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" User Session authentication mechanism
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth():
+    """ User Session Class
+    """
+    def __init__():
+        """ User Session Initialization
+        """
+        super().__init__()

3930308e31cadf40ffc0111a68012541e2fcab53
YemiDev <abodunrinsulaimon1@gmail.com>
2023-09-09 14:46:28 +0100

    Files For Testing

---
+++ 0x01-Basic_authentication/Test_Files/main_3.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Main 3
+"""
+from api.v1.auth.basic_auth import BasicAuth
+
+a = BasicAuth()
+
+print(a.decode_base64_authorization_header(None))
+print(a.decode_base64_authorization_header(89))
+print(a.decode_base64_authorization_header("Holberton School"))
+print(a.decode_base64_authorization_header("SG9sYmVydG9u"))
+print(a.decode_base64_authorization_header("SG9sYmVydG9uIFNjaG9vbA=="))
+print(a.decode_base64_authorization_header(a.extract_base64_authorization_header("Basic SG9sYmVydG9uIFNjaG9vbA==")))
+print(a.decode_base64_authorization_header(
+"SG9sYmVydG9uIFNjaG9vbCBpcyBhIHByb2plY3QtYmFzZWQgY29sbGVnZSBhbHRlcm5hdGl2ZSBmb3IgdGhlIG5leHQgZ2VuZXJhdGlvbiBvZiBzb2Z0d2FyZSBlbmdpbmVlcnM="))
+print(a.decode_base64_authorization_header("SEJUTg==")) # -> HBTN

a3ae77cf695df525249f22abd3dcefb2632d0d33
YemiDev <abodunrinsulaimon1@gmail.com>
2023-09-09 22:51:54 +0100

    Version Update

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,17 +3,27 @@
 """
 
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """ User Session Class
     """
-    def __init__():
-    """ User Session-Initialization
-    """
-    super().__init__()
+    user_id_by_session_id = {}
+
+    def __init__(self):
+        """ User Session-Initialization
+        """
+        super().__init__()
 
     def create_session(self, user_id: str = None) -> str:
         """ Function that create session
         """
-        pass
+        if user_id is None:
+            return None
+        elif not isinstance(user_id, str):
+            return None
+        else:
+            session_id = str(uuid4())
+            self.user_id_by_session_id = {session_id: user_id}
+            return session_id # self.user_id_by_session_id

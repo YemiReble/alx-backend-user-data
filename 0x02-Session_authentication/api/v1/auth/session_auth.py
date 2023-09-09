c7636cc55c47d35e095a26dcd050bebd1ff84447
YemiDev <abodunrinsulaimon1@gmail.com>
2023-09-09 19:01:20 +0100

    New Class Created for User Session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -8,7 +8,12 @@ from api.v1.auth.auth import Auth
 class SessionAuth(Auth):
     """ User Session Class
     """
-    # def __init__():
+    def __init__():
     """ User Session-Initialization
     """
-    #    super().__init__()
+    super().__init__()
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Function that create session
+        """
+        pass

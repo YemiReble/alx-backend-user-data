fa68cb1c9060b73031502af01126e1ccb82c8b1f
YemiDev <abodunrinsulaimon1@gmail.com>
2023-09-11 22:09:24 +0100

    User Schema

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -6,6 +6,9 @@ SQLalcheme model for User
 
 import sqlalchemy
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
 class User(Base):
@@ -16,5 +19,9 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     email = Column(String(250))
     hashed_pasword = Column(String(250))
-    session_id = (String(250), default=None)
-    reset_token = (String(250), default=None)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
+
+    # def __repr__(self):
+    #    return "<User(name='%s', fullname='%s', nickname='%s')>" % (
+    # self.email, self.hashed_password, self.session_id, self.reset_token)

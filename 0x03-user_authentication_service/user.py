cca012de0c5a4302e0f6a18b83347f4e000df9bb
YemiDev <abodunrinsulaimon1@gmail.com>
2023-09-12 15:50:08 +0100

    User Data

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -5,7 +5,7 @@ SQLalcheme model for User
 
 
 import sqlalchemy
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -17,7 +17,7 @@ class User(Base):
     __tablename__ = 'user'
 
     id = Column(Integer, primary_key=True)
-    email = Column(String(250))
+    email = Column(String(250), unique=True)
     hashed_pasword = Column(String(250))
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)

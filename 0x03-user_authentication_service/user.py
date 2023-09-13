baed1fa06dd938c604d72e78bdb89e04d8c160be
YemiDev <abodunrinsulaimon1@gmail.com>
2023-09-13 17:01:32 +0100

    Some Modification done on the add_user method

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -15,7 +15,7 @@ Base = declarative_base()
 class User(Base):
     """ User class declaration
     """
-    __tablename__ = 'user'
+    __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
     email = Column(String(250), unique=True)

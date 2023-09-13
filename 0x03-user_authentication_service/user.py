a322054ab14b931b567d6349782d13dac959e53d
YemiDev <abodunrinsulaimon1@gmail.com>
2023-09-13 16:54:24 +0100

    Some Modification done on the add_user method

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -5,6 +5,7 @@ SQLalcheme model for User
 
 
 import sqlalchemy
+# from sqlalchemy
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -18,6 +19,6 @@ class User(Base):
 
     id = Column(Integer, primary_key=True)
     email = Column(String(250), unique=True)
-    hashed_pasword = Column(String(250))
+    hash_password = Column(String(250))
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)

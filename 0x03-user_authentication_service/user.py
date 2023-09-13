6d9574d5e8d5e23420a79cdb9f82b939d0bebc96
YemiDev <abodunrinsulaimon1@gmail.com>
2023-09-13 17:06:47 +0100

    Some Modification done on the add_user method

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -4,9 +4,7 @@ SQLalcheme model for User
 """
 
 
-import sqlalchemy
-# from sqlalchemy
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()

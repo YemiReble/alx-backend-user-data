bf9408a451c738ae7e44e0ab66bf30d318fa971d
YemiDev <abodunrinsulaimon1@gmail.com>
2023-09-16 22:29:55 +0100

    User Registration Function

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -4,17 +4,33 @@ A basic Flask app
 """
 
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index():
-    """ a simple index
+    """ A simple index
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def add_users():
+    """ This handles the users by adding new users
+    """
+    # user = request.get_json()
+    email = request.form['email']
+    password = request.form['password']
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":

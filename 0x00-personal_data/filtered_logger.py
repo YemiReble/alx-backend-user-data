172fe758e9b0c8d0d6f19391e0da48448aa9bb87
Sulaimon <abodunrinsulaimon1@gmail.com>
2023-11-02 16:47:14 +0100

    Fildered Logger Program

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,7 +9,7 @@ from typing import List
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
+    """ Return User data filtered message """
     for field in fields:
         pattern = f"{field}=[^{separator}]*"
-        message = re.sub(pattern, f"{field}={redaction}", message)
-    return message
+    return re.sub(pattern, f"{field}={redaction}", message)

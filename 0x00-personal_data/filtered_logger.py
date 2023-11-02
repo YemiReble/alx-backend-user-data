e47c5450b7362164d62bab4fd267da512112a04b
Sulaimon <abodunrinsulaimon1@gmail.com>
2023-11-02 16:33:11 +0100

    Fildered Logger Program

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,9 +4,10 @@ A program that use re.sub to perform the
 substitution with a single regex.python3
 """
 import re
+form typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields, redaction, message, separator) -> List[str]:
     for field in fields:
         pattern = f"{field}=[^{separator}]*"
         message = re.sub(pattern, f"{field}={redaction}", message)
